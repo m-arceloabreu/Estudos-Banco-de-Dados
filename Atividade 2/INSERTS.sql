744e8403d49de0fa63ebe1c2d3f87c2fb6984784
Marcelo Abreu <marceloabreu1997@gmail.com>
2022-02-11 18:56:24 -0300

    Atividade 2

---
+++ Atividade 2/INSERTS.sql
@@ -0,0 +1,11 @@
+insert into produtos (nome,valor,descricao,marca)values("XPTO",850,"BLA BLA,BLA BLA,BLA BLA,BLA BLA,BLA BLA","Marca A");
+insert into produtos (nome,valor,descricao,marca)values("XPTO12334",1200,"BLA BLA,BLA BLA,BLA BLA,BLA BLA,BLA BLA","Marca C");
+insert into produtos (nome,valor,descricao,marca)values("XPTOXI3IO",300,"BLA BLA,BLA BLA,BLA BLA,BLA BLA,BLA BLA","Marca B");
+insert into produtos (nome,valor,descricao,marca)values("XPTOTO",250,"BLA BLA,BLA BLA,BLA BLA,BLA BLA,BLA BLA","Marca D");
+insert into produtos (nome,valor,descricao,marca)values("XPTOXP",150,"BLA BLA,BLA BLA,BLA BLA,BLA BLA,BLA BLA","Marca A");
+insert into produtos (nome,valor,descricao,marca)values("X",950,"BLA BLA,BLA BLA,BLA BLA,BLA BLA,BLA BLA","Marca D");
+insert into produtos (nome,valor,descricao,marca)values("XP",1000,"BLA BLA,BLA BLA,BLA BLA,BLA BLA,BLA BLA","Marca C");
+insert into produtos (nome,valor,descricao,marca)values("XPT",1500,"BLA BLA,BLA BLA,BLA BLA,BLA BLA,BLA BLA","Marca B");
+
+SELECT * FROM produtos where valor > 500;
+select * from produtos where valor < 500;
---
+++ Atividade 2/UPDATES-SELECT.sql
@@ -0,0 +1,3 @@
+
+update produtos set valor = 10000 where id = 1;
+select * from produtos
